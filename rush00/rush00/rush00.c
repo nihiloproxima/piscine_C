f4d4cc1efc50c3c7560fa746cefdea55b0c72f23
Nathan P <nathanplouvier60@gmail.com>
2018-08-02 22:32:19 +0200

    opti rush00

--- rush00/rush00/rush00.c
+++ rush00/rush00/rush00.c
@@ -3,29 +3,15 @@
 /*                                                              /             */
 /*   rush00.c                                         .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: naplouvi <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*   By: naplouvi <naplouvi@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/07 14:46:47 by naplouvi     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/07 16:55:36 by naplouvi    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/08/02 22:29:02 by naplouvi    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
-
-void	print_line(int width, int height, int x, int y)
-{
-	if ((width == 1 && height == 1) || (width == x && height == 1) ||
-		(width == 1 && height == y) || (width == x && height == y))
-		ft_putchar('o');
-	else if (width > 1 && width < x && height > 1 && height < y)
-		ft_putchar(' ');
-	else if ((width > 1 && height == 1 && width < x)
-			|| (height == y && width > 1 && width < x))
-		ft_putchar('-');
-	else
-		ft_putchar('|');
-}
 
 void	rush(int x, int y)
 {
@@ -39,7 +25,12 @@ void	rush(int x, int y)
 		{
 			width = 0;
 			while (width++ < x)
-				print_line(width, height, x, y);
+			{
+				if (height == 1 || height == y)
+					width > 1 && width < x ? ft_putchar('-') : ft_putchar('o');
+				else
+					width > 1 && width < x ? ft_putchar(' ') : ft_putchar('|');
+			}
 			ft_putchar('\n');
 		}
 	}
